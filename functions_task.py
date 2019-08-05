5168e3a055283400f452632f061b3fc066525b90
Sarah <sweerna1@gmail.com>
2019-08-05 21:55:03 +0300

    Functions Task

---
+++ functions_task.py
@@ -0,0 +1,35 @@
+from datetime import date
+print("FUNCTIONS TASK")
+print("The Age Calculator")
+
+def check_birthdate(y, m, d):
+    today = date.today()
+    if int(today.year) > int(y) or int(today.year) == int(y):
+        return True
+    else:
+        return False
+
+def calculate_age(y, m, d):
+    birth_year = int(today.year) - int(y)
+    birth_month = int(today.month) - int(m)
+    birth_day = int(today.day) - int(d)
+    print("You are " + str(birth_year) + " years, " + str(birth_month) + " months, and " + str(birth_day) + " days")
+
+today = date.today()
+print ("Today's date is: ", today)
+print ("----------------------------")
+
+year = input("Enter year of birth: ")
+month = input("Enter month of birth: ")
+day = input("Enter day of birth: ")
+if check_birthdate(year, month, day):
+    calculate_age(year, month, day)
+else:
+    print("You are in the future :)")
+
+
+
+
+    
+    
+    
